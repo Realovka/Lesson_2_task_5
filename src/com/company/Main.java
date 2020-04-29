bbf1728bde0eda58a7e6d2e9aef959b5a780cfe7
Nastya <realovka@mail.ru>
2020-04-29 18:25:42 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i;
+        for (i = 1; i <= 20; i++) {
+            System.out.println(i+"дюйм(-ов) "+i*2.54+" см");
+
+        }
+    }
+}
